25203f1f21e3d6815bb558bb0f8474622a5f8e73
Cedgo <cedgo1997@gmail.com>
2021-01-29 22:50:56 -0400

    card y dex component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './views/details/details.component';
 import { HomeComponent } from './views/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { DexComponent } from './components/dex/dex.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DetailsComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    DexComponent
   ],
   imports: [
     BrowserModule,
