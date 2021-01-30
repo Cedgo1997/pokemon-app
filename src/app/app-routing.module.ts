1a5030553c83c6939398c8cf011262ff77b180ec
Cedgo <cedgo1997@gmail.com>
2021-01-30 01:23:43 -0400

    home page and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DetailsComponent } from "./views/details/details.component";
+import { HomeComponent } from "./views/home/home.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "details/:id", component: DetailsComponent },
+  { path: "**", pathMatch: "full", redirectTo: "home" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
