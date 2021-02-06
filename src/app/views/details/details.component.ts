e74ed2be6acf0bd0778188a7b6a6d085551d2197
Cedgo <cedgo1997@gmail.com>
2021-02-06 00:36:13 -0400

    activated router added, info fetched

--- src/app/views/details/details.component.ts
+++ src/app/views/details/details.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PokemonService } from "src/app/services/pokemon.service";
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.scss"],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {
+    this.getPokemon();
   }
 
+  pokemonData;
+  ngOnInit(): void {}
+
+  getPokemon() {
+    const id = this.route.snapshot.paramMap.get("id");
+    this.pokemonService
+      .getPokemon(id)
+      .then((m) => m.json())
+      .then((p) => console.log(p));
+  }
 }
