c73624d036d0875f9b8f46d84f8fd2b4b34f1614
Cedgo <cedgo1997@gmail.com>
2021-02-06 03:59:07 -0400

    sprites and change pokemon functions added

--- src/app/components/dex/dex.component.ts
+++ src/app/components/dex/dex.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-dex',
-  templateUrl: './dex.component.html',
-  styleUrls: ['./dex.component.scss']
+  selector: "app-dex",
+  templateUrl: "./dex.component.html",
+  styleUrls: ["./dex.component.scss"],
 })
 export class DexComponent implements OnInit {
+  @Input() data: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    console.log(this.data);
   }
 
+  ngOnInit(): void {}
+
+  changePokemon(id: number | string) {
+    this.router.navigate(["details", id]);
+  }
+
+  nextPokemon() {
+    if (this.data.id + 1 > 898) return;
+    this.router.navigate(["details", this.data.id + 1]);
+  }
+  previousPokemon() {
+    if (this.data.id - 1 < 1) return;
+    this.router.navigate(["details", this.data.id - 1]);
+  }
+
+  goHome() {
+    this.router.navigate(["/"]);
+  }
 }
